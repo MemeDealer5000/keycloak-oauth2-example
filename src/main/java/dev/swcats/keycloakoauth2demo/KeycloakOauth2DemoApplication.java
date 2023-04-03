aefbb151b345dbb6f5885c2c55bb765474f8099f
s.ravnushkin <s.ravnushkin@swcats.dev>
2023-04-03 09:37:11 +0500

    Initial commit

---
+++ src/main/java/dev/swcats/keycloakoauth2demo/KeycloakOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.swcats.keycloakoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakOauth2DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/swcats/keycloakoauth2demo/KeycloakOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.swcats.keycloakoauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakOauth2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
